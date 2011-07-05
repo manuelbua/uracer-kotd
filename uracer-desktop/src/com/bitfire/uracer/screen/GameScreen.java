83269f45f3f4aa1589edc69b033b59080de34b68
Manuel Bua <manuel.bua@gmail.com>
2011-07-05 22:24:17 +0200

    Simple Input testcase.

--- uracer-desktop/src/com/bitfire/uracer/screen/GameScreen.java
+++ uracer-desktop/src/com/bitfire/uracer/screen/GameScreen.java
@@ -1,6 +1,7 @@
 package com.bitfire.uracer.screen;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.bitfire.uracer.Art;
@@ -23,6 +24,11 @@ public class GameScreen extends Screen
 	}
 
 	@Override
+	public void tick( Input input )
+	{
+	}
+
+	@Override
 	public void render(float timeAliasingFactor)
 	{
 		GL20 gl = Gdx.graphics.getGL20();
@@ -34,16 +40,19 @@ public class GameScreen extends Screen
 
 		spriteBatch.draw( Art.titleScreen, 0, 0 );
 
-		int h = Gdx.graphics.getHeight();
-		drawString( "FPS: " + Gdx.graphics.getFramesPerSecond(), 5, h-30 );
-		drawString( "dt: " + Gdx.graphics.getDeltaTime(), 5, h-23 );
-		drawString( "ta: " + timeAliasingFactor, 5, h-16 );
+			int h = Gdx.graphics.getHeight();
+			drawString( "FPS: " + Gdx.graphics.getFramesPerSecond(), 5, h-30 );
+			drawString( "dt: " + Gdx.graphics.getDeltaTime(), 5, h-23 );
+			drawString( "ta: " + timeAliasingFactor, 5, h-16 );
+
+			// input test case
+			drawString( "[space] isOn = " + Input.isOn( Keys.SPACE ), 0, 100 );
+			drawString( "[space] isOff = " + Input.isOff( Keys.SPACE ), 0, 107 );
+			drawString( "[space] isPressed = " + Input.isPressed( Keys.SPACE ), 0, 114 );
+			drawString( "[space] isReleased = " + Input.isReleased( Keys.SPACE ), 0, 121 );
+
+			drawString( "gdx = " + Gdx.input.isKeyPressed( Keys.SPACE ), 0, 128 );
 
 		spriteBatch.end();
-	}
-
-	@Override
-	public void tick( Input input )
-	{
 	}
 }
