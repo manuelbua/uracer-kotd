15992bc65ed56a702beb0d8e666850dd5f40a3b8
Manuel Bua <manuel.bua@gmail.com>
2011-07-06 22:27:26 +0200

    (formatting)

--- uracer-desktop/src/com/bitfire/uracer/Input.java
+++ uracer-desktop/src/com/bitfire/uracer/Input.java
@@ -2,12 +2,11 @@ package com.bitfire.uracer;
 
 import com.badlogic.gdx.InputAdapter;
 
-
 public class Input extends InputAdapter
 {
 	// keys
 	private static int[] buttons = new int[ 256 ];
-	private static int[] old_buttons = new int[ 256 ];	// unused
+	private static int[] old_buttons = new int[ 256 ]; // unused
 
 	// touches
 	private static int touchX = 0;
@@ -15,30 +14,66 @@ public class Input extends InputAdapter
 	private static boolean is_touching = false;
 	private static boolean is_dragging = false;
 
-	private static final int FLAG_REAL_ON = ( 1 << 0 );
-	private static final int FLAG_DELAY_ON = ( 1 << 1 );
-	private static final int FLAG_CUR_ON = ( 1 << 2 );
-	private static final int FLAG_LAST_ON = ( 1 << 3 );
-
+	private static final int FLAG_REAL_ON = (1 << 0);
+	private static final int FLAG_DELAY_ON = (1 << 1);
+	private static final int FLAG_CUR_ON = (1 << 2);
+	private static final int FLAG_LAST_ON = (1 << 3);
 
 	//
 	// game interface
 	//
 
-	public static boolean isTouching() { return is_touching; }
-	public static boolean isDragging() { return is_dragging; }
-	public static int getX() { return touchX; }
-	public static int getY() { return touchY; }
+	public static boolean isTouching()
+	{
+		return is_touching;
+	}
 
-	public static boolean isOn( int keycode )	{ return is(keycode, FLAG_CUR_ON); }
-	public static boolean isOff( int keycode )	{ return !isOn(keycode); }
-	public static boolean wasOn( int keycode )	{ return is(keycode, FLAG_LAST_ON); }
-	public static boolean wasOff( int keycode )	{ return !wasOn(keycode); }
+	public static boolean isDragging()
+	{
+		return is_dragging;
+	}
 
-	public static boolean isPressed( int keycode )	{ return( isOn(keycode) && wasOff(keycode) ); }
-	public static boolean isReleased( int keycode )	{ return( isOff(keycode) && wasOn(keycode) ); }
+	public static int getX()
+	{
+		return touchX;
+	}
 
-	private static boolean is(int keycode, int flag)
+	public static int getY()
+	{
+		return touchY;
+	}
+
+	public static boolean isOn( int keycode )
+	{
+		return is( keycode, FLAG_CUR_ON );
+	}
+
+	public static boolean isOff( int keycode )
+	{
+		return !isOn( keycode );
+	}
+
+	public static boolean wasOn( int keycode )
+	{
+		return is( keycode, FLAG_LAST_ON );
+	}
+
+	public static boolean wasOff( int keycode )
+	{
+		return !wasOn( keycode );
+	}
+
+	public static boolean isPressed( int keycode )
+	{
+		return (isOn( keycode ) && wasOff( keycode ));
+	}
+
+	public static boolean isReleased( int keycode )
+	{
+		return (isOff( keycode ) && wasOn( keycode ));
+	}
+
+	private static boolean is( int keycode, int flag )
 	{
 		return ((buttons[keycode] & flag) == flag);
 	}
@@ -67,22 +102,20 @@ public class Input extends InputAdapter
 			if( (flag & FLAG_CUR_ON) == FLAG_CUR_ON )
 			{
 				buttons[i] |= FLAG_LAST_ON;
-			}
-			else
+			} else
 			{
 				buttons[i] &= ~FLAG_LAST_ON;
 			}
 
-		    if( (flag & (FLAG_DELAY_ON | FLAG_REAL_ON)) == (FLAG_DELAY_ON | FLAG_REAL_ON) )
-		    {
-		    	buttons[i] |= FLAG_CUR_ON;
-		    }
-		    else
-		    {
-		    	buttons[i] &= ~FLAG_CUR_ON;
-		    }
+			if( (flag & (FLAG_DELAY_ON | FLAG_REAL_ON)) == (FLAG_DELAY_ON | FLAG_REAL_ON) )
+			{
+				buttons[i] |= FLAG_CUR_ON;
+			} else
+			{
+				buttons[i] &= ~FLAG_CUR_ON;
+			}
 
-		    buttons[i] &= ~FLAG_DELAY_ON;
+			buttons[i] &= ~FLAG_DELAY_ON;
 		}
 	}
 
@@ -94,7 +127,6 @@ public class Input extends InputAdapter
 		}
 	}
 
-
 	//
 	// from InputProcessor
 	//
@@ -102,15 +134,15 @@ public class Input extends InputAdapter
 	@Override
 	public boolean keyDown( int keycode )
 	{
-		buttons[ keycode ] |= (FLAG_REAL_ON | FLAG_DELAY_ON);
-//		buttons[ keycode ] |= (FLAG_REAL_ON | FLAG_CUR_ON);
+		buttons[keycode] |= (FLAG_REAL_ON | FLAG_DELAY_ON);
+		// buttons[ keycode ] |= (FLAG_REAL_ON | FLAG_CUR_ON);
 		return false;
 	}
 
 	@Override
 	public boolean keyUp( int keycode )
 	{
-		buttons[ keycode ] &= ~FLAG_REAL_ON;
+		buttons[keycode] &= ~FLAG_REAL_ON;
 		return false;
 	}
 
