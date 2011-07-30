3d5208b796129bcabb46a9a8d4831e6aecc655a9
Manuel Bua <manuel.bua@gmail.com>
2011-07-30 21:56:33 +0200

    (testing version info)

--- uracer-desktop/src/com/bitfire/uracer/VersionInfo.java
+++ uracer-desktop/src/com/bitfire/uracer/VersionInfo.java
@@ -2,6 +2,5 @@ package com.bitfire.uracer;
 
 public class VersionInfo
 {
-	public int versionCode = 0;
-	public String versionName = "v0.0.4-2-g95c1acc";
+	public String versionName = "v0.0.4-3-g88d719b";
 }
