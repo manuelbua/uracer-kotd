869c97afc07aee8f08e03e60be9c15df81343f36
Manuel Bua <manuel.bua@gmail.com>
2011-08-04 22:14:53 +0200

    Cleanup

--- uracer-desktop/src/com/bitfire/uracer/VersionInfo.java
+++ uracer-desktop/src/com/bitfire/uracer/VersionInfo.java
@@ -2,6 +2,6 @@ package com.bitfire.uracer;
 
 public class VersionInfo
 {
-	public static String versionName = "v0.0.5-3-g2b422f6/incorp-tilemap";
+	public static String versionName = "v0.0.5-6-g4e992db/fix-inverted-y";
 }
 
