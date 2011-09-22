1f568cbe922280b3629771c06dbf707b119a4aa4
Manuel Bua <manuel.bua@gmail.com>
2011-09-22 21:07:31 +0200

    Add new font: Curse, Free (from dafont.com)

--- uracer-desktop/src/com/bitfire/uracer/VersionInfo.java
+++ uracer-desktop/src/com/bitfire/uracer/VersionInfo.java
@@ -2,6 +2,6 @@ package com.bitfire.uracer;
 
 public class VersionInfo
 {
-	public static String versionName = "v0.1.0-11-g17f5241/master";
+	public static String versionName = "v0.1.0-12-g0aefc00/master";
 }
 
