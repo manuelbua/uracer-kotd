555e972aa0a33ff9d1b018d2e24dc3ed9e460095
Manuel Bua <manuel.bua@gmail.com>
2011-09-24 15:18:46 +0200

    Add script for resource building.

--- uracer-desktop/src/com/bitfire/uracer/VersionInfo.java
+++ uracer-desktop/src/com/bitfire/uracer/VersionInfo.java
@@ -2,6 +2,6 @@ package com.bitfire.uracer;
 
 public class VersionInfo
 {
-	public static String versionName = "v0.1.0-20-g33a9c22/refactoring";
+	public static String versionName = "v0.1.0-21-ga8e2803/refactoring";
 }
 
