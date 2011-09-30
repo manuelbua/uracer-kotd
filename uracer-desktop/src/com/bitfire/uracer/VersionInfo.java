27c19da9756a504ee97952d791fee0c4849c291c
Manuel Bua <manuel.bua@gmail.com>
2011-09-30 22:41:55 +0200

    Rename to CarDrift.

--- uracer-desktop/src/com/bitfire/uracer/VersionInfo.java
+++ uracer-desktop/src/com/bitfire/uracer/VersionInfo.java
@@ -2,6 +2,6 @@ package com.bitfire.uracer;
 
 public class VersionInfo
 {
-	public static String versionName = "v0.1.1-22-gc96c7c0/master";
+	public static String versionName = "v0.1.1-22-g80d4f23/master";
 }
 
--- uracer-desktop/src/com/bitfire/uracer/effects/CarDrift.java
+++ uracer-desktop/src/com/bitfire/uracer/effects/CarDrift.java
@@ -2,25 +2,30 @@ package com.bitfire.uracer.effects;
 
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.bitfire.uracer.Art;
+import com.bitfire.uracer.Director;
 import com.bitfire.uracer.Physics;
 import com.bitfire.uracer.entities.vehicles.Car;
 import com.bitfire.uracer.simulations.car.CarModel;
 import com.bitfire.uracer.utils.Convert;
 
-public class CarDrifts
+public class CarDrift
 {
 	public Sprite front, rear;
 	public float life;
 	public float maxLife;
+	public boolean visible;
 	private float lifeRatio;
 	private float alphaFront, alphaRear;
+	public Vector2 position;
 
-	public CarDrifts( Car player )
+	public CarDrift( Car player )
 	{
 		front = new Sprite();
 		rear = new Sprite();
+		position = new Vector2();
 
 		// setup sprites
 		CarModel model = player.getCarModel();
@@ -38,6 +43,7 @@ public class CarDrifts
 		rear.setColor( 1, 1, 1, 0 );
 
 		life = maxLife = 0;
+		visible = false;
 	}
 
 	public void tick()
@@ -45,9 +51,11 @@ public class CarDrifts
 		if( life > 0 )
 		{
 			life -= Physics.dt;
+			visible = Director.isVisible( getBoundingRectangle() );
 		} else
 		{
 			life = 0;
+			visible = false;
 		}
 	}
 
@@ -81,6 +89,7 @@ public class CarDrifts
 
 	public void setPosition( Vector2 pos )
 	{
+		position.set( pos );
 		front.setPosition( pos.x - front.getOriginX(), pos.y - front.getOriginY() );
 		rear.setPosition( pos.x - rear.getOriginX(), pos.y - rear.getOriginY() );
 	}
@@ -89,6 +98,14 @@ public class CarDrifts
 	{
 		front.setRotation( degrees );
 		rear.setRotation( degrees );
+	}
+
+	private Rectangle tmp = new Rectangle();
+	public Rectangle getBoundingRectangle()
+	{
+		tmp.set( front.getBoundingRectangle() );
+		tmp.merge( rear.getBoundingRectangle() );
+		return tmp;
 	}
 
 	public void updateRatio()
