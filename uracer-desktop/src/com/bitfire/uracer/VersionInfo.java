0ec2a5324304ae5320c7b45fd2e8fbfd12ffe65a
Manuel Bua <manuel.bua@gmail.com>
2011-10-01 17:47:35 +0200

    Add some palms!

--- uracer-desktop/src/com/bitfire/uracer/VersionInfo.java
+++ uracer-desktop/src/com/bitfire/uracer/VersionInfo.java
@@ -2,6 +2,6 @@ package com.bitfire.uracer;
 
 public class VersionInfo
 {
-	public static String versionName = "v0.1.1-28-g7b34f44/master";
+	public static String versionName = "v0.1.1-29-g32abaf9/master";
 }
 
