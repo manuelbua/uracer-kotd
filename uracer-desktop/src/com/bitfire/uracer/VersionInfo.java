2687290555c386c4329c9db46874d35f3c00a817
Manuel Bua <manuel.bua@gmail.com>
2011-10-02 22:39:25 +0200

    [fixed] DriftInfo was being invalidated incorrectly.

--- uracer-desktop/src/com/bitfire/uracer/VersionInfo.java
+++ uracer-desktop/src/com/bitfire/uracer/VersionInfo.java
@@ -2,6 +2,6 @@ package com.bitfire.uracer;
 
 public class VersionInfo
 {
-	public static String versionName = "v0.1.1-41-gb4d5fb1/master";
+	public static String versionName = "v0.1.1-42-g4e201bd/master";
 }
 
--- uracer-desktop/src/com/bitfire/uracer/simulations/car/CarContactListener.java
+++ uracer-desktop/src/com/bitfire/uracer/simulations/car/CarContactListener.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.physics.box2d.ContactListener;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.Manifold;
 import com.bitfire.uracer.entities.vehicles.Car;
+import com.bitfire.uracer.game.logic.DriftInfo;
 import com.bitfire.uracer.utils.Box2DUtils;
 
 public class CarContactListener implements ContactListener
@@ -27,16 +28,24 @@ public class CarContactListener implements ContactListener
 	}
 
 	Vector2 tmp = new Vector2();
+
 	private void addImpactFeedback( Fixture f, ContactImpulse impulse )
 	{
-		if( (Box2DUtils.isCar(f) || Box2DUtils.isGhostCar(f)) && f.getBody() != null )
+		if( (Box2DUtils.isCar( f ) || Box2DUtils.isGhostCar( f )) && f.getBody() != null )
 		{
 			Car car = (Car)f.getBody().getUserData();
 			tmp.set( impulse.getNormalImpulses()[0], impulse.getNormalImpulses()[1] );
 			float res = tmp.len();
 
 			car.addImpactFeedback( res );
-//			System.out.println("Impact data =" + res );
+
+			// update DriftInfo in case of collision
+			if( car.getInputMode() == CarInputMode.InputFromPlayer )
+			{
+				DriftInfo.invalidateByCollision();
+			}
+
+			// System.out.println("Impact data =" + res );
 		}
 	}
 
