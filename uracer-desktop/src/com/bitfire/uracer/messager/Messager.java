f4e57b76293e351fe7ddd1b0d1e8cd4c7821315f
Manuel Bua <manuel.bua@gmail.com>
2011-10-02 22:40:05 +0200

    Add support for multiple concurrent messages, if they are being shown within different groups (Top, Middle, Bottom).

--- uracer-desktop/src/com/bitfire/uracer/messager/Messager.java
+++ uracer-desktop/src/com/bitfire/uracer/messager/Messager.java
@@ -1,9 +1,9 @@
 package com.bitfire.uracer.messager;
 
 import java.util.LinkedList;
-import java.util.Queue;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Array;
 
 public class Messager
 {
@@ -23,8 +23,8 @@ public class Messager
 	}
 
 	// data
-	private static Queue<Message> messages;
-	private static Message current;
+	private static Array<LinkedList<Message>> messages;
+	private static Array<Message> currents;
 
 	private Messager()
 	{
@@ -32,8 +32,15 @@ public class Messager
 
 	public static void init()
 	{
-		current = null;
-		messages = new LinkedList<Message>();
+		currents = new Array<Message>( 3 );
+		currents.insert( MessagePosition.Top.ordinal(), null );
+		currents.insert( MessagePosition.Middle.ordinal(), null );
+		currents.insert( MessagePosition.Bottom.ordinal(), null );
+
+		messages = new Array<LinkedList<Message>>( 3 );
+		messages.insert( MessagePosition.Top.ordinal(), new LinkedList<Message>() );
+		messages.insert( MessagePosition.Middle.ordinal(), new LinkedList<Message>() );
+		messages.insert( MessagePosition.Bottom.ordinal(), new LinkedList<Message>() );
 	}
 
 	public static void dispose()
@@ -41,70 +48,87 @@ public class Messager
 		reset();
 	}
 
-	public static boolean isBusy()
+	public static boolean isBusy( MessagePosition group )
 	{
-		return (current != null);
+		return (currents.get( group.ordinal() ) != null);
 	}
 
 	public static void reset()
 	{
+		messages.get( MessagePosition.Top.ordinal() ).clear();
+		messages.get( MessagePosition.Middle.ordinal() ).clear();
+		messages.get( MessagePosition.Bottom.ordinal() ).clear();
+
+		currents.clear();
 		messages.clear();
-		current = null;
+
+		init();
+
 		// System.out.println("Messages just got cleaned up.");
 	}
 
 	public static void tick()
 	{
+		update( MessagePosition.Top );
+		update( MessagePosition.Middle );
+		update( MessagePosition.Bottom );
+	}
+
+	private static void update( MessagePosition group )
+	{
+		LinkedList<Message> msgs = messages.get( group.ordinal() );
+		Message msg = currents.get( group.ordinal() );
+
 		// any message?
-		if( !isBusy() && (messages.peek() != null) )
+		if( msg == null && (msgs.peek() != null) )
 		{
 			// schedule this message to be processed next
-			current = messages.remove();
+			msg = msgs.remove();
+			currents.set( group.ordinal(), msg );
 		}
 
 		// busy or became busy?
-		if( isBusy() )
+		if( msg != null )
 		{
 			// start message if needed
-			if( !current.started )
+			if( !msg.started )
 			{
-				current.started = true;
-				current.startMs = System.currentTimeMillis();
-				current.onShow();
+				msg.started = true;
+				msg.startMs = System.currentTimeMillis();
+				msg.onShow();
 			}
 
-			if( !current.tick() )
+			if( !msg.tick() )
 			{
-				current = null;
+				currents.set( group.ordinal(), null );
 				return;
 			}
 
 			// check if finished
-			if( (System.currentTimeMillis() - current.startMs) >= current.durationMs && !current.isHiding() )
+			if( (System.currentTimeMillis() - msg.startMs) >= msg.durationMs && !msg.isHiding() )
 			{
 				// message should end
-				current.onHide();
+				msg.onHide();
 			}
 		}
 	}
 
 	public static void render( SpriteBatch batch )
 	{
-		if( isBusy() )
-		{
-			current.render( batch );
-		}
+		if( isBusy( MessagePosition.Top ) ) currents.get( MessagePosition.Top.ordinal() ).render( batch );
+		if( isBusy( MessagePosition.Middle ) ) currents.get( MessagePosition.Middle.ordinal() ).render( batch );
+		if( isBusy( MessagePosition.Bottom ) ) currents.get( MessagePosition.Bottom.ordinal() ).render( batch );
 	}
 
 	public static void show( String message, float durationSecs, MessageType type, MessagePosition position, MessageSize size )
 	{
-		if( isBusy() ) current.onHide();
+		if( isBusy(position) ) currents.get( position.ordinal() ).onHide();
 		enqueue( message, durationSecs, type, position, size );
 	}
 
 	public static void enqueue( String message, float durationSecs, MessageType type, MessagePosition position, MessageSize size )
 	{
 		Message m = new Message( message, durationSecs, type, position, size );
-		messages.add( m );
+		messages.get( position.ordinal() ).add( m );
 	}
 }
