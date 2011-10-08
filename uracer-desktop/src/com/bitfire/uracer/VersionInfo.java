023ac2aa28ba6d7b16d023cbb484f3561cb5fea3
Manuel Bua <manuel.bua@gmail.com>
2011-10-08 18:01:43 +0200

    Updated meshes and textures.

--- uracer-desktop/src/com/bitfire/uracer/VersionInfo.java
+++ uracer-desktop/src/com/bitfire/uracer/VersionInfo.java
@@ -2,6 +2,6 @@ package com.bitfire.uracer;
 
 public class VersionInfo
 {
-	public static String versionName = "v0.1.2-3-g2e8f0f4/master";
+	public static String versionName = "v0.1.2-4-geb4f29d/master";
 }
 
