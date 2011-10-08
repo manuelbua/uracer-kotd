0b168f49438b205f18b893fbe9cc114ab7d709b5
Manuel Bua [laptop] <manuel.bua@gmail.com>
2011-10-08 18:17:05 +0200

    Bump version.

--- uracer-desktop/src/com/bitfire/uracer/VersionInfo.java
+++ uracer-desktop/src/com/bitfire/uracer/VersionInfo.java
@@ -2,6 +2,6 @@ package com.bitfire.uracer;
 
 public class VersionInfo
 {
-	public static String versionName = "v0.1.2-5/merge";
+	public static String versionName = "v0.1.2-9-g8dd3245/master";
 }
 
