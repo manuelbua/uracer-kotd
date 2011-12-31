ad44586db3c8b79435979de9c672dcc06e63601b
Manuel Bua <manuel.bua@gmail.com>
2011-12-31 15:33:10 +0100

    Specify custom Android configuration: do NOT use accelerometer and compass, request GLES2 (it should be made mandatory).

--- uracer-android/src/com/bitfire/uracer/URacerAndroid.java
+++ uracer-android/src/com/bitfire/uracer/URacerAndroid.java
@@ -3,6 +3,7 @@ package com.bitfire.uracer;
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
 public class URacerAndroid extends AndroidApplication
 {
@@ -10,6 +11,11 @@ public class URacerAndroid extends AndroidApplication
 	public void onCreate( Bundle savedInstanceState )
 	{
 		super.onCreate( savedInstanceState );
-		initialize( new URacer(), true );
+
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useAccelerometer = false;
+		config.useCompass = false;
+		config.useGL20 = true;
+		initialize( new URacer(), config );
 	}
 }
