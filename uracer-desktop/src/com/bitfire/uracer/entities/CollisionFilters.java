b93411f32aaafc97abedb0be77cc11b5a8145e46
Manuel Bua <manuel.bua@gmail.com>
2012-01-15 12:04:17 +0100

    Update TODO, add useful comments.

--- uracer-desktop/src/com/bitfire/uracer/entities/CollisionFilters.java
+++ uracer-desktop/src/com/bitfire/uracer/entities/CollisionFilters.java
@@ -1,5 +1,25 @@
 package com.bitfire.uracer.entities;
 
+/**
+ * Defines entities' behavior upon collision: this follows the box2d rules (see b2WorldCallbacks.cpp)
+ *
+ * bool b2ContactFilter::ShouldCollide(b2Fixture* fixtureA, b2Fixture* fixtureB)
+ * {
+ * 		const b2Filter& filterA = fixtureA->GetFilterData();
+ * 		const b2Filter& filterB = fixtureB->GetFilterData();
+ *
+ * 		if (filterA.groupIndex == filterB.groupIndex && filterA.groupIndex != 0)
+ * 		{
+ * 			return filterA.groupIndex > 0;
+ * 		}
+ *
+ * 		bool collide = (filterA.maskBits & filterB.categoryBits) != 0 && (filterA.categoryBits & filterB.maskBits) != 0;
+ * 		return collide;
+ * }
+
+ * @author manuel
+ *
+ */
 public class CollisionFilters
 {
 	public static final short GroupNoCollisions = 	-1;
