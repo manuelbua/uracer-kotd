5b2add883601462731aa3ff0d3627feed2537580
Manuel Bua <manuel.bua@gmail.com>
2012-02-06 15:33:32 +0100

    Introduced the CarImpactManager.

--- uracer-desktop/src/com/bitfire/uracer/carsimulation/CarContactListener.java
+++ uracer-desktop/src/com/bitfire/uracer/carsimulation/CarContactListener.java
@@ -1,26 +1,27 @@
 package com.bitfire.uracer.carsimulation;
 
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Contact;
 import com.badlogic.gdx.physics.box2d.ContactImpulse;
 import com.badlogic.gdx.physics.box2d.ContactListener;
-import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.Manifold;
-import com.bitfire.uracer.audio.CarSoundManager;
-import com.bitfire.uracer.entities.vehicles.Car;
-import com.bitfire.uracer.game.logic.DriftInfo;
 import com.bitfire.uracer.utils.Box2DUtils;
 
 public class CarContactListener implements ContactListener
 {
+	private CarImpactManager impactManager = new CarImpactManager();
+
 	@Override
 	public void beginContact( Contact contact )
 	{
+		if(Box2DUtils.isCar(contact.getFixtureA()) || Box2DUtils.isCar(contact.getFixtureB()))
+			System.out.println("beginContact");
 	}
 
 	@Override
 	public void endContact( Contact contact )
 	{
+		if(Box2DUtils.isCar(contact.getFixtureA()) || Box2DUtils.isCar(contact.getFixtureB()))
+			System.out.println("endContact");
 	}
 
 	@Override
@@ -28,40 +29,9 @@ public class CarContactListener implements ContactListener
 	{
 	}
 
-	Vector2 tmp = new Vector2();
-
-	private void addImpactFeedback( Fixture f, ContactImpulse impulse )
-	{
-		if( (Box2DUtils.isCar( f ) || Box2DUtils.isGhostCar( f )) && f.getBody() != null )
-		{
-			Car car = (Car)f.getBody().getUserData();
-			float[] impulses = impulse.getNormalImpulses();
-			tmp.set( impulses[0], impulses[1] );
-
-			float res = tmp.len();
-			if( res > 0 )
-			{
-				car.addImpactFeedback( res );
-
-				// update DriftInfo in case of collision
-				if( car.getInputMode() == CarInputMode.InputFromPlayer )
-				{
-//					System.out.println( "Impact data = " + res );
-
-					CarSoundManager.carImpacted( res );
-					DriftInfo.invalidateByCollision();
-				}
-			}
-		}
-	}
-
 	@Override
 	public void postSolve( Contact contact, ContactImpulse impulse )
 	{
-		Fixture a = contact.getFixtureA();
-		Fixture b = contact.getFixtureB();
-
-		addImpactFeedback( a, impulse );
-		addImpactFeedback( b, impulse );
+		impactManager.impact( contact, impulse );
 	}
 }
---
+++ uracer-desktop/src/com/bitfire/uracer/carsimulation/CarImpactManager.java
@@ -0,0 +1,64 @@
+package com.bitfire.uracer.carsimulation;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.bitfire.uracer.audio.CarSoundManager;
+import com.bitfire.uracer.entities.vehicles.Car;
+import com.bitfire.uracer.game.logic.DriftInfo;
+import com.bitfire.uracer.utils.Box2DUtils;
+
+public class CarImpactManager
+{
+	private long lastImpactTime, prevImpactTime;
+
+	public CarImpactManager()
+	{
+		lastImpactTime = prevImpactTime = 0;
+	}
+
+	public void impact(Contact contact, ContactImpulse impulse)
+	{
+		Fixture a = contact.getFixtureA();
+		Fixture b = contact.getFixtureB();
+
+		addImpactFeedback( a, impulse );
+		addImpactFeedback( b, impulse );
+
+	}
+
+	private Vector2 tmpVec2 = new Vector2();
+	private void addImpactFeedback( Fixture f, ContactImpulse impulse )
+	{
+		if( (Box2DUtils.isCar( f ) || Box2DUtils.isGhostCar( f )) && f.getBody() != null )
+		{
+			Car car = (Car)f.getBody().getUserData();
+			float[] impulses = impulse.getNormalImpulses();
+			tmpVec2.set( impulses[0], impulses[1] );
+
+			float len2 = tmpVec2.len2();
+			if( len2 > 0 )
+			{
+				float len = (float)Math.sqrt( len2 );
+				car.addImpactFeedback( len );
+
+				// update DriftInfo in case of collision
+				if( car.getInputMode() == CarInputMode.InputFromPlayer )
+				{
+					impactByPlayer( car, len );
+				}
+			}
+		}
+	}
+
+	private void impactByPlayer(Car car, float impulseLength)
+	{
+		CarSoundManager.carImpacted( impulseLength );
+		DriftInfo.invalidateByCollision();
+
+
+		prevImpactTime = lastImpactTime;
+		lastImpactTime = System.currentTimeMillis();
+	}
+}
