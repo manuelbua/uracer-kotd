26f388e627a961f7be92c92e786fb5daf09eb81e
Manuel Bua <manuel.bua@gmail.com>
2012-03-11 17:20:12 +0100

    [added] old resolution back for more testing.

--- uracer-desktop/src/com/bitfire/uracer/URacerDesktop.java
+++ uracer-desktop/src/com/bitfire/uracer/URacerDesktop.java
@@ -33,6 +33,7 @@ public class URacerDesktop
 		config.title = "uRacer: The King Of The Drift";
 
 		config.width = 1920; config.height = 1050;
+//		config.width = 1730; config.height = 1016;
 //		config.width = 1280; config.height = 752;
 
 //		config.width = 1280; config.height = 720;
