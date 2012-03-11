1b0a2252a4cdfbf4fb57d31fe77f8692a0e40981
Manuel Bua <manuel.bua@gmail.com>
2012-03-12 00:58:06 +0100

    [added] new bilinear precomputed 3x3 kernel blur.

--- uracer-desktop/src/com/bitfire/uracer/effects/postprocessing/filters/Blur.java
+++ uracer-desktop/src/com/bitfire/uracer/effects/postprocessing/filters/Blur.java
@@ -24,8 +24,9 @@ public class Blur
 	public enum BlurType
 	{
 		Gaussian3x3( Tap.Tap3x3 ),
+		Gaussian3x3b(Tap.Tap3x3 ),	// R=5 (11x11, policy "higher-then-discard")
 		Gaussian5x5( Tap.Tap5x5 ),
-		Gaussian5x5b( Tap.Tap5x5 ), // R=9 (19x19)
+		Gaussian5x5b( Tap.Tap5x5 ), // R=9 (19x19, policy "higher-then-discard")
 		;
 
 		public final Tap tap;
@@ -112,6 +113,9 @@ public class Blur
 		float[] outOffsetsH = c.offsetsHor;
 		float[] outOffsetsV = c.offsetsVert;
 
+		float dx = this.invWidth;
+		float dy = this.invHeight;
+
 		switch( this.type )
 		{
 		default:
@@ -124,23 +128,41 @@ public class Blur
 			computeOffsets( this.type.tap.radius, this.invWidth, this.invHeight, outOffsetsH, outOffsetsV );
 			break;
 
+		case Gaussian3x3b:
+			// weights and offsets are computed from a binomial distribution
+			// and reduced to be used *only* with bilinearly-filtered texture lookups
+			//
+			// with radius = 1f
+
+			// weights
+			outWeights[0] = 0.352941f;
+			outWeights[1] = 0.294118f;
+			outWeights[2] = 0.352941f;
+
+			// horizontal offsets
+			outOffsetsH[0] = -1.33333f;	outOffsetsH[1] = 0f;
+			outOffsetsH[2] = 0f;		outOffsetsH[3] = 0f;
+			outOffsetsH[4] = 1.33333f;	outOffsetsH[5] = 0f;
+
+			// vertical offsets
+			outOffsetsV[0] = 0f;	outOffsetsV[1] = -1.33333f;
+			outOffsetsV[2] = 0f;	outOffsetsV[3] = 0f;
+			outOffsetsV[4] = 0f;	outOffsetsV[5] = 1.33333f;
+
+			// scale offsets from binomial space to screen space
+			for(int i = 0; i < c.length * 2; i++) {
+				outOffsetsH[i] *= dx;
+				outOffsetsV[i] *= dy;
+			}
+
+			break;
+
 		case Gaussian5x5b:
 
 			// weights and offsets are computed from a binomial distribution
 			// and reduced to be used *only* with bilinearly-filtered texture lookups
 			//
 			// with radius = 2f
-			// with rtt ratio = 0.25f
-
-			float dx = this.invWidth;
-			float dy = this.invHeight;
-
-			// ---------------8<---------------8<---------------8<---------------8<---------------8<---------------8<
-			// weights and offsets are computed from a binomial distribution
-			// and reduced to be used *only* with bilinearly-filtered texture lookups
-			//
-			// with radius = 2f
-			// with rtt ratio = 0.25f
 
 			// weights
 			outWeights[0] = 0.0702703f;
@@ -169,7 +191,6 @@ public class Blur
 				outOffsetsH[i] *= dx;
 				outOffsetsV[i] *= dy;
 			}
-			// ---------------8<---------------8<---------------8<---------------8<---------------8<---------------8<
 
 			break;
 		}
