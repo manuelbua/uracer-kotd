b3c42b4088e837b188b7500a5a0399dad688f7c2
Manuel Bua <manuel.bua@gmail.com>
2012-03-11 17:20:45 +0100

    [added] single-dimension convolve filter.

---
+++ uracer-desktop/src/com/bitfire/uracer/effects/postprocessing/filters/Convolve1D.java
@@ -0,0 +1,46 @@
+package com.bitfire.uracer.effects.postprocessing.filters;
+
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+import com.bitfire.uracer.utils.ShaderLoader;
+
+public class Convolve1D
+{
+	public final int length;
+	public float[] weights;
+	public float[] offsets;
+
+	protected ShaderProgram convolve1d;
+
+	public Convolve1D(int length)
+	{
+		this(length, new float[length], new float[length*2]);
+	}
+
+	public Convolve1D(int length, float[] weights)
+	{
+		this(length, weights, new float[length*2]);
+	}
+
+	public Convolve1D(int length, float[] weights, float[] offsets)
+	{
+		this.length = length;
+		convolve1d = ShaderLoader.createShader("bloom/convolve-1d", "bloom/convolve-1d", "#define LENGTH " + length);
+		this.weights = weights;
+		this.offsets = offsets;
+	}
+
+	public void dispose()
+	{
+		convolve1d.dispose();
+		weights = offsets = null;
+	}
+
+	public void upload()
+	{
+		convolve1d.begin();
+		convolve1d.setUniform1fv( "SampleWeights", weights, 0, length );
+		convolve1d.setUniform2fv( "SampleOffsets", offsets, 0, length*2  /* libgdx ask for number of floats! */ );
+		convolve1d.end();
+	}
+
+}
