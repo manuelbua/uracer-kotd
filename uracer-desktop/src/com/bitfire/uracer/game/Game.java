manuelbua/uracer-kotd

commit 2d9a568f1d89c63a864395c6a8223c05eebe3493

Refactoring. Reset DriftInfo information whenever a reset/restart occurs. Separate the Hud drifting logic from the whole Hud logic. Also, resets the hud Drifting component, responsible for showing drift-related events. Enhance the HudLabel so that support scaling and alpha operations.

uracer-desktop/src/com/bitfire/uracer/game/logic/DriftInfo.java
@@ -5,10 +5,14 @@ import com.bitfire.uracer.utils.AMath;
 
 public class DriftInfo
 {
-	public long driftTime = 0;
+	public float driftSeconds = 0;
 	public boolean isDrifting = false;
+	public boolean hasCollided = false;
 	public float lateralForcesFront = 0, lateralForcesRear = 0, lastRear, lastFront;
 	public float driftStrength;
+
+	private long driftStartTime = 0;
+	private long collisionTime;
 
 	private static DriftInfo instance = null;
 	private static GameLogic logic;
@@ -24,6 +28,29 @@ public class DriftInfo
 		return instance;
 	}
 
+	public void reset()
+	{
+		lastFront = lastRear = 0;
+		driftSeconds = 0;
+		hasCollided = false;
+		isDrifting = false;
+		collisionTime = 0;
+		lateralForcesFront = lateralForcesRear = 0;
+		driftStrength = 0;
+	}
+
+	public static void invalidateByCollision()
+	{
+		DriftInfo drift = DriftInfo.get();
+		if( !drift.isDrifting ) return;
+
+		drift.collisionTime = System.currentTimeMillis();
+		drift.isDrifting = false;
+		drift.hasCollided = true;
+		drift.updateDriftTimeSeconds();
+		logic.getListener().onEndDrift();
+	}
+
 	public void update( Car player )
 	{
 		lastFront = lateralForcesFront;
@@ -36,25 +63,47 @@ public class DriftInfo
 		driftStrength = AMath.clamp(
 				((Math.abs( lateralForcesFront ) + Math.abs( lateralForcesRear )) * 0.5f) / player.getCarModel().max_grip, 0, 1 );
 
-		if( !isDrifting )
+		if( isDrifting )
 		{
-			// search for onBeginDrift
-			if( driftStrength > 0.4f )
+			// update in-drift time
+			updateDriftTimeSeconds();
+		}
+
+		if( hasCollided )
+		{
+			// ignore drifts for a couple of seconds
+			if( System.currentTimeMillis() - collisionTime > 1000 )
 			{
-				isDrifting = true;
-				driftTime = System.currentTimeMillis();
-				logic.getListener().onBeginDrift();
+				hasCollided = false;
 			}
 		} else
 		{
-			// search for onEndDrift
-			if( isDrifting && driftStrength < 0.2f )
+			if( !isDrifting )
 			{
-				driftTime = System.currentTimeMillis() - driftTime;
-				isDrifting = false;
-				logic.getListener().onEndDrift();
+				// search for onBeginDrift
+				if( driftStrength > 0.4f && player.getCarDescriptor().velocity_wc.len() > 20 )
+				{
+					isDrifting = true;
+					hasCollided = false;
+					driftStartTime = System.currentTimeMillis();
+					updateDriftTimeSeconds();
+					logic.getListener().onBeginDrift();
+				}
+			} else
+			{
+				// search for onEndDrift
+				if( isDrifting && driftStrength < 0.2f )
+				{
+					isDrifting = false;
+					hasCollided = false;
+					logic.getListener().onEndDrift();
+				}
 			}
 		}
+	}
 
+	private void updateDriftTimeSeconds()
+	{
+		driftSeconds = (System.currentTimeMillis() - driftStartTime) / 1000f;
 	}
 }

uracer-android/AndroidManifest.xml
@@ -1,8 +1,8 @@
 <?xml version="1.0" encoding="utf-8"?>
 <manifest xmlns:android="http://schemas.android.com/apk/res/android"
       package="com.bitfire.uracer"
-      android:versionCode="180"
-      android:versionName="v0.1.1-43-g2687290/master">
+      android:versionCode="181"
+      android:versionName="v0.1.1-44-gf4e57b7/master">
     <supports-screens android:anyDensity="true" android:xlargeScreens="true" />
     <uses-sdk android:minSdkVersion="7" />
     <application android:icon="@drawable/icon" android:label="@string/app_name">

uracer-desktop/src/com/bitfire/uracer/VersionInfo.java
@@ -2,6 +2,6 @@ package com.bitfire.uracer;
 
 public class VersionInfo
 {
-	public static String versionName = "v0.1.1-43-g2687290/master";
+	public static String versionName = "v0.1.1-44-gf4e57b7/master";
 }
 

uracer-desktop/src/com/bitfire/uracer/game/logic/GameLogic.java
@@ -55,7 +55,7 @@ public class GameLogic
 			PostProcessor.setEffect( rb );
 		}
 
-		DriftInfo.init(this);
+		DriftInfo.init( this );
 		reset();
 	}
 
@@ -105,6 +105,8 @@ public class GameLogic
 
 	public void restart()
 	{
+		tweener.clear();
+
 		// causes an onTileChanged event to be raised
 		lastCarTileAt.set( -1, -1 );
 		carTileAt.set( lastCarTileAt );

uracer-desktop/src/com/bitfire/uracer/game/logic/GameLogicListener.java
@@ -38,6 +38,8 @@ public class GameLogicListener implements IGameLogicListener
 	public void onReset()
 	{
 		lapInfo.reset();
+		DriftInfo.get().reset();
+		if(logic.getGame().getHud()!=null) logic.getGame().getHud().getDrifting().reset();
 		isFirstLap = true;
 		lastRecordedLapId = 0;
 	}
@@ -46,6 +48,8 @@ public class GameLogicListener implements IGameLogicListener
 	public void onRestart()
 	{
 		isFirstLap = true;
+		DriftInfo.get().reset();
+		if(logic.getGame().getHud()!=null) logic.getGame().getHud().getDrifting().reset();
 		Messager.show( "WARM  UP  LAP", 3f, MessageType.Information, MessagePosition.Middle, MessageSize.Big );
 	}
 
@@ -107,13 +111,13 @@ public class GameLogicListener implements IGameLogicListener
 					{
 						lapInfo.setLastTrackTimeSeconds( best.trackTimeSeconds );
 						Messager.show( "-" + String.format( "%.4f", worst.trackTimeSeconds - best.trackTimeSeconds )
-								+ " seconds!", 3f, MessageType.Good, MessagePosition.Middle, MessageSize.Big );
+								+ " seconds!", 3f, MessageType.Good, MessagePosition.Top, MessageSize.Big );
 					} else
 					{
 						lapInfo.setLastTrackTimeSeconds( worst.trackTimeSeconds );
 						Messager.show(
 								"+" + String.format( "%.4f", worst.trackTimeSeconds - best.trackTimeSeconds ) + " seconds", 3f,
-								MessageType.Bad, MessagePosition.Middle, MessageSize.Big );
+								MessageType.Bad, MessagePosition.Top, MessageSize.Big );
 					}
 
 					ghost.setReplay( best );
@@ -129,12 +133,13 @@ public class GameLogicListener implements IGameLogicListener
 	@Override
 	public void onBeginDrift()
 	{
-		System.out.println("--> begin drift");
+		logic.getGame().getHud().getDrifting().onBeginDrift();
+//		System.out.println( "--> begin drift" );
 	}
 
 	@Override
 	public void onEndDrift()
 	{
-		System.out.println("end drift (" + String.format( "%.02f", DriftInfo.get().driftTime/1000f ) + " seconds) <--");
+		logic.getGame().getHud().getDrifting().onEndDrift();
 	}
 }

uracer-desktop/src/com/bitfire/uracer/hud/Hud.java
@@ -23,6 +23,9 @@ public class Hud
 	private HudDebugMeter meterLatForce, meterSkidMarks;
 	private Matrix4 topLeftOrigin, identity;
 
+	// components
+	private HudDrifting hudDrift;
+
 	// effects
 	public Hud( GameLogic logic )
 	{
@@ -37,9 +40,13 @@ public class Hud
 		// grid-based position
 		int gridX = (int)((float)Gdx.graphics.getWidth() / 5f);
 
+		// laptimes component
 		best = new HudLabel( Art.fontCurseYR, "BEST  TIME\n-.----" );
 		curr = new HudLabel( Art.fontCurseYR, "YOUR  TIME\n-.----" );
 		last = new HudLabel( Art.fontCurseYR, "LAST  TIME\n-.----" );
+
+		// drifting component
+		hudDrift = new HudDrifting( logic );
 
 		curr.setPosition( gridX - curr.getBounds().width, 10 );
 		last.setPosition( gridX * 4 - last.getBounds().width, 10 );
@@ -61,6 +68,11 @@ public class Hud
 		return logic;
 	}
 
+	public HudDrifting getDrifting()
+	{
+		return hudDrift;
+	}
+
 	public void dispose()
 	{
 	}
@@ -68,6 +80,7 @@ public class Hud
 	public void tick()
 	{
 		Messager.tick();
+		hudDrift.tick();
 	}
 
 	private void updateLapTimes()
@@ -119,19 +132,27 @@ public class Hud
 		best.render( batch );
 		last.render( batch );
 
+		// dbg only
 		if( Config.isDesktop )
 		{
 			DriftInfo drift = DriftInfo.get();
 
 			// lateral forces
 			meterLatForce.setValue( drift.driftStrength );
-			if(drift.isDrifting) meterLatForce.color.set( .3f, 1f, .3f, 1f );
-			else meterLatForce.color.set( 1f, 1f, 1f, 1f );
+
+			if( drift.isDrifting )
+				meterLatForce.color.set( .3f, 1f, .3f, 1f );
+			else
+				meterLatForce.color.set( 1f, 1f, 1f, 1f );
+
 			meterLatForce.render( batch );
 
 			meterSkidMarks.setValue( TrackEffects.getParticleCount( Effects.CarSkidMarks ) );
 			meterSkidMarks.render( batch );
 		}
+
+		// render drifting component
+		hudDrift.render( batch );
 
 		batch.end();
 	}

uracer-desktop/src/com/bitfire/uracer/hud/HudDrifting.java
@@ -1,0 +1,157 @@
+package com.bitfire.uracer.hud;
+
+import aurelienribon.tweenengine.Tween;
+import aurelienribon.tweenengine.TweenCallback;
+import aurelienribon.tweenengine.TweenGroup;
+import aurelienribon.tweenengine.equations.Expo;
+
+import com.badlogic.gdx.graphics.g2d.BitmapFont.TextBounds;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.bitfire.uracer.Art;
+import com.bitfire.uracer.Director;
+import com.bitfire.uracer.entities.vehicles.Car;
+import com.bitfire.uracer.game.logic.DriftInfo;
+import com.bitfire.uracer.game.logic.GameLogic;
+import com.bitfire.uracer.messager.Messager;
+import com.bitfire.uracer.messager.Messager.MessagePosition;
+import com.bitfire.uracer.messager.Messager.MessageSize;
+import com.bitfire.uracer.messager.Messager.MessageType;
+import com.bitfire.uracer.simulations.car.CarModel;
+import com.bitfire.uracer.tweenables.TweenHudLabel;
+import com.bitfire.uracer.utils.Convert;
+
+public class HudDrifting
+{
+	private GameLogic logic;
+	private Car player;
+	private CarModel model;
+	private int carWidthPx, carLengthPx;
+
+	private HudLabel labelRealtime, labelResult;
+	private TweenHudLabel tweenRealtime, tweenResult;
+	private TextBounds boundsRealtime, boundsResult;
+	private DriftInfo drift;
+
+	public HudDrifting( GameLogic logic )
+	{
+		this.logic = logic;
+		this.player = logic.getGame().getLevel().getPlayer();
+		this.model = player.getCarModel();
+		carWidthPx = (int)Convert.mt2px( model.width );
+		carLengthPx = (int)Convert.mt2px( model.length );
+
+		labelRealtime = new HudLabel( Art.fontCurseYRbig, "99.99" );
+		labelRealtime.setScale( 0.5f, true );
+		labelRealtime.setAlpha( 0 );
+		boundsRealtime = labelRealtime.getBounds();
+		tweenRealtime = new TweenHudLabel( labelRealtime );
+
+		labelResult = new HudLabel( Art.fontCurseR, "99.99" );
+		labelResult.setAlpha( 0 );
+		boundsResult = labelResult.getBounds();
+		tweenResult = new TweenHudLabel( labelResult );
+	}
+
+	public void reset()
+	{
+		labelRealtime.setAlpha( 0 );
+		labelResult.setAlpha( 0 );
+	}
+
+	public void tick()
+	{
+		drift = DriftInfo.get();
+	}
+
+	public void render( SpriteBatch batch )
+	{
+		Vector2 p = Director.screenPosForPx( player.state().position );
+		Vector2 h = player.getSimulator().heading;
+
+		//
+		// draw earned seconds
+		//
+
+		labelRealtime.setString( "+" + String.format( "%.02f", drift.driftSeconds ) );
+		labelRealtime.setPosition( p.x - labelRealtime.getBounds().width * 0.5f, p.y - labelRealtime.getBounds().height * 0.5f );
+		labelRealtime.x -= h.x * (carWidthPx + labelRealtime.getBounds().width * 0.5f);		// offset by heading
+		labelRealtime.y -= h.y * (carLengthPx + labelRealtime.getBounds().height * 0.5f);		// offset by heading
+		labelRealtime.render( batch );
+
+
+		//
+		// draw lost seconds
+		//
+		labelResult.setPosition( p.x - labelResult.getBounds().width * 0.5f, p.y - labelResult.getBounds().height * 0.5f );
+		labelResult.x -= h.x * (carWidthPx + labelResult.getBounds().width * 0.5f);		// offset by heading
+		labelResult.y -= h.y * (carLengthPx + labelResult.getBounds().height * 0.5f);		// offset by heading
+		labelResult.render( batch );
+	}
+
+	public void onBeginDrift()
+	{
+		GameLogic.getTweener().add( Tween.to( tweenRealtime, TweenHudLabel.OPACITY, 500, Expo.INOUT ).target( 1f ) );
+	}
+
+	public void onEndDrift()
+	{
+		if( drift.hasCollided )
+		{
+			// ended due to collision?
+			labelResult.setString( "-" + String.format( "%.02f", drift.driftSeconds ) );
+			labelResult.setFont( Art.fontCurseR );
+
+			GameLogic.getTweener().add(
+				TweenGroup.sequence(
+					TweenGroup.parallel(
+						Tween.to( tweenRealtime, TweenHudLabel.OPACITY, 500, Expo.INOUT ).target( 0f ),
+						Tween.to( tweenResult, TweenHudLabel.OPACITY, 500, Expo.INOUT ).target( 1f )
+					),
+					Tween.call( new TweenCallback() {
+						@Override
+						public void tweenEventOccured( Types eventType, Tween tween )
+						{
+							GameLogic.getTweener().add( Tween.to( tweenResult, TweenHudLabel.OPACITY, 500, Expo.INOUT ).target( 0f ) );
+						}
+					}).delay( 500 )
+				)
+			);
+		}
+		else
+		{
+			labelResult.setString( "+" + String.format( "%.02f", drift.driftSeconds ) );
+			labelResult.setFont( Art.fontCurseG );
+
+			GameLogic.getTweener().add(
+				TweenGroup.sequence(
+					TweenGroup.parallel(
+						Tween.to( tweenRealtime, TweenHudLabel.OPACITY, 500, Expo.INOUT ).target( 0f ),
+						Tween.to( tweenResult, TweenHudLabel.OPACITY, 500, Expo.INOUT ).target( 1f )
+					),
+					Tween.call( new TweenCallback() {
+						@Override
+						public void tweenEventOccured( Types eventType, Tween tween )
+						{
+							GameLogic.getTweener().add( Tween.to( tweenResult, TweenHudLabel.OPACITY, 500, Expo.INOUT ).target( 0f ) );
+						}
+					}).delay( 500 )
+				)
+			);
+
+			if( drift.driftSeconds > 1 && drift.driftSeconds < 1.5f )
+			{
+				Messager.enqueue( "NICE ONE!\n+" + String.format( "%.02f", drift.driftSeconds ) + "  seconds!", 1f,
+						MessageType.Good, MessagePosition.Bottom, MessageSize.Big );
+			} else if( drift.driftSeconds >= 1.5f && drift.driftSeconds < 2f )
+			{
+				Messager.enqueue( "FANTASTIC!\n+" + String.format( "%.02f", drift.driftSeconds ) + "  seconds!", 1f,
+						MessageType.Good, MessagePosition.Bottom, MessageSize.Big );
+			} else if( drift.driftSeconds >= 2f )
+			{
+				Messager.enqueue( "U N R E A L!\n+" + String.format( "%.02f", drift.driftSeconds ) + "  seconds!", 1f,
+						MessageType.Good, MessagePosition.Bottom, MessageSize.Big );
+			}
+		}
+	}
+}

uracer-desktop/src/com/bitfire/uracer/hud/HudLabel.java
@@ -3,30 +3,47 @@ package com.bitfire.uracer.hud;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.BitmapFont.TextBounds;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.bitfire.uracer.Art;
 
 public class HudLabel
 {
+	public float x, y;
+	public float alpha;
+
 	private String what;
 	private TextBounds bounds;
 	private BitmapFont font;
-	private float x, y;
+	private float scale;
 
 	public HudLabel( BitmapFont font, String string )
 	{
-		what = string;
 		this.font = font;
 		bounds = new TextBounds();
+		what = string;
+		alpha = 1f;
+		scale = 1f;
 		recomputeBounds();
 	}
 
 	public void setString( String string )
 	{
+		setString( string, false );
+	}
+
+	public void setString( String string, boolean computeBounds )
+	{
 		what = string;
+		if( computeBounds ) recomputeBounds();
+	}
+
+	public void setPosition( float posX, float posY )
+	{
+		x = posX;
+		y = posY;
 	}
 
 	public void recomputeBounds()
 	{
+		font.setScale( scale );
 		bounds.set( font.getMultiLineBounds( what ) );
 	}
 
@@ -35,14 +52,41 @@ public class HudLabel
 		return bounds;
 	}
 
-	public void setPosition( float x, float y )
+	public float getAlpha()
 	{
-		this.x = x;
-		this.y = y;
+		return alpha;
+	}
+
+	public void setAlpha( float value )
+	{
+		alpha = value;
+	}
+
+	public void setFont( BitmapFont font )
+	{
+		this.font = font;
+		recomputeBounds();
+	}
+
+	public void setScale( float scale )
+	{
+		setScale( scale, false );
+	}
+
+	public void setScale( float scale, boolean recomputeBounds )
+	{
+		this.scale = scale;
+		if( recomputeBounds ) recomputeBounds();
 	}
 
 	public void render( SpriteBatch batch )
 	{
-		Art.fontCurseYR.drawMultiLine( batch, what, x, y );
+		if( alpha > 0 )
+		{
+			font.setScale( scale );
+			font.setColor( 1, 1, 1, alpha );
+			font.drawMultiLine( batch, what, x, y );
+			font.setColor( 1, 1, 1, 1 );
+		}
 	}
 }

commit 0c982a69a5b8fa1db5a4e38606ef5105ca7eb52f

[fixed] drift time wasn't consistent, (bug introduced in 2d9a568f1d89c63a864395c6a8223c05eebe3493) [experimenting] factors, effects, bloom and zoom..

uracer-desktop/src/com/bitfire/uracer/game/Game.java
@@ -138,13 +138,13 @@ public class Game {
 		bloomSettings = new Bloom.Settings( "subtle", Config.PostProcessing.BlurType, 1, 1.5f, threshold, 1f, 0.5f, 1f, 1.5f );
 		bloom.setSettings( bloomSettings );
 
-		// zoom = new Zoom( Config.PostProcessing.ZoomQuality );
+		 zoom = new Zoom( Config.PostProcessing.ZoomQuality );
 
-		// postProcessor.addEffect( zoom );
+		 postProcessor.addEffect( zoom );
 		postProcessor.addEffect( bloom );
 	}
 
-	private float prevFactor = 0;
+	private float prevFactor = 0, prevDriftTimeFactor = 0;
 
 	public boolean tick() {
 		if( !logic.tick() ) return false;
@@ -154,23 +154,24 @@ public class Game {
 		CarSoundManager.tick();
 
 		// post-processor debug ------------------------------
-		float factor = DriftInfo.get().driftStrength * 2f;
-//		 float factor = player.currSpeedFactor * 1.75f;
-		 factor = 1f;
+//		float factor = player.currSpeedFactor * 1.75f;
 
-		factor = AMath.lerp( prevFactor, factor, 0.125f );
+		float factor = AMath.lerp( prevFactor, DriftInfo.get().driftStrength * 2f, 0.05f );
 		prevFactor = factor;
 
-		// fade bloom
+		float driftTimeFactor = !DriftInfo.get().isDrifting ? 0 : AMath.clamp( DriftInfo.get().driftSeconds / 2f /* seconds */, 0, 1.5f);
+		driftTimeFactor = AMath.lerp( prevDriftTimeFactor, driftTimeFactor, 0.0025f );
+		prevDriftTimeFactor = driftTimeFactor;
+
 		if( Config.Graphics.EnablePostProcessingFx && bloom != null ) {
-			bloom.setBaseSaturation( bloomSettings.baseSaturation + 0.5f * (1-factor) );
-			bloom.setBloomIntesity( bloomSettings.bloomIntensity * factor );
+			bloom.setBaseSaturation( bloomSettings.baseSaturation + 0.5f * (1-driftTimeFactor) );
+			bloom.setBloomIntesity( bloomSettings.bloomIntensity * driftTimeFactor );
 		}
 
-//		if( Config.Graphics.EnablePostProcessingFx && zoom != null ) {
-//			zoom.setOrigin( Director.screenPosFor( player.car.getBody() ) );
-//			zoom.setStrength( -0.1f * factor );
-//		}
+		if( Config.Graphics.EnablePostProcessingFx && zoom != null ) {
+			zoom.setOrigin( Director.screenPosFor( player.car.getBody() ) );
+			zoom.setStrength( -0.1f * factor * 0.1f );
+		}
 //
 //		if( Config.Graphics.EnablePostProcessingFx && bloom != null && zoom != null ) {
 //			bloom.setBaseSaturation( 0.5f - 0.8f * factor );

uracer-desktop/src/com/bitfire/uracer/game/logic/DriftInfo.java
@@ -85,13 +85,13 @@ public class DriftInfo {
 					isDrifting = true;
 					hasCollided = false;
 					driftStartTime = System.currentTimeMillis();
-					updateDriftTimeSeconds();
 					logic.getListener().onBeginDrift();
 				}
 			}
 			else {
 				// search for onEndDrift
 				if( isDrifting && (driftStrength < 0.2f || vel < 15f) ) {
+					updateDriftTimeSeconds();
 					isDrifting = false;
 					hasCollided = false;
 					logic.getListener().onEndDrift();