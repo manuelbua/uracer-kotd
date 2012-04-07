6f1b5a5b729fa1f7632dd6164ea5643b71fac7eb
Manuel Bua <manuel.bua@gmail.com>
2012-04-08 00:06:07 +0200

    Refactored GameData, added new public construction helpers. GameLogic lightened up, most of the components manages themselves now.

--- uracer-desktop/src/com/bitfire/uracer/game/GameData.java
+++ uracer-desktop/src/com/bitfire/uracer/game/GameData.java
@@ -3,15 +3,17 @@ package com.bitfire.uracer.game;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
 import com.bitfire.uracer.Config;
+import com.bitfire.uracer.effects.TrackEffects;
 import com.bitfire.uracer.entities.vehicles.Car;
 import com.bitfire.uracer.factories.CarFactory;
 import com.bitfire.uracer.game.collisions.GameContactListener;
 import com.bitfire.uracer.game.logic.DriftState;
 import com.bitfire.uracer.game.logic.LapState;
+import com.bitfire.uracer.game.logic.PhysicsStep;
 import com.bitfire.uracer.game.logic.PlayerState;
-import com.bitfire.uracer.hud.Hud;
 import com.bitfire.uracer.hud.HudLabel;
 import com.bitfire.uracer.messager.Message;
+import com.bitfire.uracer.messager.Messager;
 import com.bitfire.uracer.tiled.ScalingStrategy;
 import com.bitfire.uracer.tweener.BoxedFloat;
 import com.bitfire.uracer.tweener.Tweener;
@@ -27,40 +29,66 @@ public class GameData {
 	public static ScalingStrategy scalingStrategy;
 	public static GameplaySettings gameSettings;
 	public static GameWorld gameWorld;
+	public static Messager messager;
 
 	public static Tweener tweener;
 	public static World b2dWorld;
-	public static Hud hud;
 
-	// state
-	public static PlayerState playerState;
-	public static DriftState driftState;
-	public static LapState lapState;
+	public static final class State {
+		public static PlayerState playerState;
+		public static DriftState driftState;
+		public static LapState lapState;
+
+		public static void dispose() {
+			playerState.dispose();
+			driftState.dispose();
+		}
+	}
+
+	public static final class System {
+		public static PhysicsStep physicsStep;
+		public static TrackEffects trackEffects;
+
+		public static void dispose() {
+			trackEffects.dispose();
+		}
+	}
 
 	public static void create( GameDifficulty difficulty ) {
-		// computed for a 256px tile size target (need conversion)
+		// computed for a 256px tile size target (compute needed conversion factors)
 		scalingStrategy = new ScalingStrategy( new Vector2( 1280, 800 ), 70f, 224, 1f );
 
 		// everything has been setup on a 256px tile, scale back if that's the case
 		Config.Physics.PixelsPerMeter /= (GameData.scalingStrategy.targetScreenRatio / GameData.scalingStrategy.to256);
 
+		GameData.gameSettings = new GameplaySettings( difficulty );
 		GameData.tweener = createTweener();
 		GameData.b2dWorld = createBox2DWorld();
-
-		GameData.gameSettings = new GameplaySettings( difficulty );
+		GameData.messager = new Messager();
 	}
 
-	public static void createStates(Car car) {
-		GameData.playerState = new PlayerState(car, CarFactory.createGhost( car ));
-		GameData.driftState = new DriftState();
-		GameData.lapState = new LapState();
+	public static void createStates( Car car ) {
+		State.playerState = new PlayerState( car, CarFactory.createGhost( car ) );
+		State.driftState = new DriftState();
+		State.lapState = new LapState();
+	}
+
+	public static void createSystems() {
+		System.physicsStep = new PhysicsStep( GameData.b2dWorld );
+		System.trackEffects = new TrackEffects( State.playerState.car );
+	}
+
+	public static void createWorld( String levelName, boolean nightMode ) {
+		gameWorld = new GameWorld( levelName, nightMode );
 	}
 
 	public static void dispose() {
 		GameData.b2dWorld.dispose();
-		GameData.hud.dispose();
 		GameData.tweener.clear();
 		GameData.tweener.dispose();
+		GameData.messager.dispose();
+		GameData.State.dispose();
+		GameData.System.dispose();
 	}
 
 	private static Tweener createTweener() {
