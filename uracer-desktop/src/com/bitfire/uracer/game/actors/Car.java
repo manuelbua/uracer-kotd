3f4e4f84da76fdce4dba80d47e0b127cf94ccf7c
Manuel Bua <manuel.bua@gmail.com>
2012-05-05 18:09:01 +0200

    [fixed] the Car's center of mass wasn't correctly initialized.
    Removes stupid global variable and accessor (TraveledDistanceDt).
    Override setWorldPosMt to track the previous position.

--- uracer-desktop/src/com/bitfire/uracer/game/actors/Car.java
+++ uracer-desktop/src/com/bitfire/uracer/game/actors/Car.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.MassData;
 import com.bitfire.uracer.configuration.Config;
 import com.bitfire.uracer.game.collisions.CollisionFilters;
 import com.bitfire.uracer.game.rendering.GameRendererEvent;
@@ -44,7 +45,6 @@ public abstract class Car extends Box2DEntity {
 	protected int impacts = 0;
 
 	private CarForces carForces = new CarForces();
-	private Vector2 carForcesVelocity = new Vector2();
 
 	// distance
 	private Vector2 previousPosition = new Vector2();
@@ -53,9 +53,6 @@ public abstract class Car extends Box2DEntity {
 	// the car's traveled distance so far, in meters
 	private float carTraveledDistance = 0;
 	private int accuDistCount = 0;
-
-	// the car's traveled distance performed in the last dt, in meters
-	private float carTraveledDistanceDt = 0;
 
 	// the car's average speed, in meters/sec, so far.
 	private float carAvgSpeedMtSec = 0;
@@ -81,7 +78,6 @@ public abstract class Car extends Box2DEntity {
 		this.impacts = 0;
 		this.inputMode = inputMode;
 		this.carTraveledDistance = 0;
-		this.carTraveledDistanceDt = 0;
 		this.accuDistCount = 0;
 		this.accuSpeed = 0;
 		this.accuSpeedCount = 0;
@@ -133,6 +129,10 @@ public abstract class Car extends Box2DEntity {
 
 		FixtureAtlas atlas = new FixtureAtlas( Gdx.files.internal( shapeName ) );
 		atlas.createFixtures( body, shapeRef, factor.x, factor.y, fd, offset, carType );
+
+		MassData mdata = body.getMassData();
+		mdata.center.set( 0, 0 );
+		body.setMassData( mdata );
 	}
 
 	/** Subclasses will feed forces to the simulator, such as Replay data stored
@@ -180,11 +180,6 @@ public abstract class Car extends Box2DEntity {
 		return carTraveledDistance;
 	}
 
-	/** Returns the traveled distance, in meters, performed in the last dt */
-	public float getTraveledDistanceDt() {
-		return carTraveledDistanceDt;
-	}
-
 	/** Returns the average speed, in meters/s, so far. */
 	public float getAverageSpeed() {
 		return carAvgSpeedMtSec;
@@ -220,7 +215,6 @@ public abstract class Car extends Box2DEntity {
 
 	public void resetTraveledDistance() {
 		carTraveledDistance = 0;
-		carTraveledDistanceDt = 0;
 		accuDistCount = 0;
 		accuSpeed = 0;
 		accuSpeedCount = 0;
@@ -253,25 +247,28 @@ public abstract class Car extends Box2DEntity {
 	@Override
 	public void setTransform( Vector2 position, float orient ) {
 		super.setTransform( position, orient );
+		previousPosition.set( body.getPosition() );
+	}
 
-		// keeps track of the previous position to be able
-		// to compute the cumulative traveled distance
+	@Override
+	public void setWorldPosMt( Vector2 worldPosition ) {
+		super.setWorldPosMt( worldPosition );
+		previousPosition.set( body.getPosition() );
+	}
+
+	@Override
+	public void setWorldPosMt( Vector2 worldPosition, float orientationRads ) {
+		super.setWorldPosMt( worldPosition, orientationRads );
 		previousPosition.set( body.getPosition() );
 	}
 
 	@Override
 	public void onBeforePhysicsSubstep() {
-		super.onBeforePhysicsSubstep();
-
 		// keeps track of the previous position to be able
 		// to compute the cumulative traveled distance
-		previousPosition.set( body.getPosition() );
+//		previousPosition.set( body.getPosition() );
 
-		// reset mt/dt
-		carTraveledDistanceDt = 0;
-
-		// reset instant speed
-		carInstantSpeedMtSec = 0;
+		super.onBeforePhysicsSubstep();
 
 		// let's subclasses behave as needed, ask them to fill carForces with new data
 		onComputeCarForces( carForces );
@@ -283,41 +280,41 @@ public abstract class Car extends Box2DEntity {
 		}
 
 		// FIXME is it really necessary? that's an expensive jni call..
-		body.setAwake( true );
+		// body.setAwake( true );
 
 		// put newly computed forces into the system
-		carForcesVelocity.set( carForces.velocity_x, carForces.velocity_y );
-		body.setLinearVelocity( carForcesVelocity );
+		body.setLinearVelocity( carForces.velocity_x, carForces.velocity_y );
 		body.setAngularVelocity( -carForces.angularVelocity );
 	}
 
 	@Override
-	public strictfp void onAfterPhysicsSubstep() {
+	public void onAfterPhysicsSubstep() {
 		super.onAfterPhysicsSubstep();
 
 		// compute traveled distance, in meters
-
 		distmp.set( body.getPosition() );
 		distmp.sub( previousPosition );
+		previousPosition.set( body.getPosition() );
 
 		// filter out zero distance
 		float dist = AMath.fixup( distmp.len() );
 		if( !AMath.isZero( dist ) ) {
-			// keeps track of it
-			carTraveledDistanceDt = dist;
-
+//			Gdx.app.log( "", "dist=" + NumberString.formatVeryLong( dist ) );
 			// accumulate distance it
 			carTraveledDistance += dist;
 			accuDistCount++;
+		} else {
+			dist = 0;
 		}
 
-		// compute instant speed, these should be valid w/ a stddev of ~0.001
-		carInstantSpeedMtSec = AMath.fixup( carTraveledDistanceDt * Config.Physics.PhysicsTimestepHz );
+		// compute instant speed
+		carInstantSpeedMtSec = AMath.fixup( dist * Config.Physics.PhysicsTimestepHz );
 		accuSpeed += carInstantSpeedMtSec;
 		accuSpeedCount++;
 
-		// compute average speed, these should be valid w/ a stddev of ~0.001
-		carAvgSpeedMtSec = AMath.fixup( accuSpeed / (float)accuSpeedCount );
+		// compute average speed
+		carAvgSpeedMtSec = AMath.fixup( accuSpeed / accuSpeedCount );
+//		Gdx.app.log( this.getClass().getSimpleName(), "avg_speed=" + NumberString.formatVeryLong(carAvgSpeedMtSec) );
 	}
 
 	@Override
