9d99e23bfdb4981804ffa5bfd3dcde6ac02de6d4
Manuel Bua <manuel.bua@gmail.com>
2012-06-10 17:41:05 +0200

    Update other color preset.

--- uracer-desktop/src/com/bitfire/uracer/game/logic/post/animators/AggressiveWarm.java
+++ uracer-desktop/src/com/bitfire/uracer/game/logic/post/animators/AggressiveWarm.java
@@ -15,58 +15,67 @@ import com.bitfire.uracer.game.rendering.GameRenderer;
 import com.bitfire.uracer.game.world.GameWorld;
 import com.bitfire.uracer.postprocessing.effects.Bloom;
 import com.bitfire.uracer.postprocessing.effects.CrtMonitor;
+import com.bitfire.uracer.postprocessing.effects.Curvature;
 import com.bitfire.uracer.postprocessing.effects.Vignette;
 import com.bitfire.uracer.postprocessing.effects.Zoom;
 import com.bitfire.uracer.resources.Art;
 import com.bitfire.uracer.utils.AMath;
 
 public final class AggressiveWarm implements Animator {
-	private GameRenderer gameRenderer = null;
 	private GameWorld gameWorld;
 	private Bloom bloom = null;
 	private Zoom zoom = null;
 	private Vignette vignette = null;
-	private CrtMonitor tv = null;
+	private CrtMonitor crt = null;
+	private Curvature curvature = null;
 
 	public AggressiveWarm( GameWorld world, PostProcessing post ) {
-		gameRenderer = post.getGameRenderer();
 		gameWorld = world;
 		bloom = (Bloom)post.getEffect( "bloom" );
 		zoom = (Zoom)post.getEffect( "zoom" );
 		vignette = (Vignette)post.getEffect( "vignette" );
-		tv = (CrtMonitor)post.getEffect( "tvlines" );
+		crt = (CrtMonitor)post.getEffect( "crt" );
+		curvature = (Curvature)post.getEffect( "curvature" );
 
 		reset();
 	}
 
 	@Override
 	public void reset() {
-		if( Config.PostProcessing.Enabled ) {
-			if( Config.PostProcessing.EnableBloom ) {
-				float threshold = ((gameWorld.isNightMode() && !Config.Graphics.DumbNightMode) ? 0.2f : 0.45f);
-				Bloom.Settings bloomSettings = new Bloom.Settings( "subtle", Config.PostProcessing.BlurType, 1, 1.5f, threshold, 1f, 0.5f, 1f, 1.5f );
-				bloom.setSettings( bloomSettings );
-			}
+		if( !Config.PostProcessing.Enabled ) {
+			return;
+		}
 
-			if( Config.PostProcessing.EnableVignetting ) {
-				vignette.setCoords( 0.8f, 0.25f );
-				vignette.setCenter( Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2 );
-				vignette.setLut( Art.postXpro );
-				vignette.setEnabled( true );
-			}
+		if( bloom != null ) {
+			float threshold = ((gameWorld.isNightMode() && !Config.Graphics.DumbNightMode) ? 0.2f : 0.45f);
+			Bloom.Settings bloomSettings = new Bloom.Settings( "subtle", Config.PostProcessing.BlurType, 1, 1.5f, threshold, 1f, 0.5f, 1f, 1.5f );
+			bloom.setSettings( bloomSettings );
+		}
 
-			if( Config.PostProcessing.EnableCrtScreen ) {
-				startMs = TimeUtils.millis();
-				tv.setTime( 0 );
+		if( vignette != null ) {
+			vignette.setCoords( 0.8f, 0.25f );
+			vignette.setCenter( Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2 );
+			vignette.setLut( Art.postXpro );
+			vignette.setLutIndex( 7 );
+			vignette.setEnabled( true );
+		}
 
-				// FIXME should find out a way to compute this per-resolution! OR!
-				// Just try manually the game and write down the offset, this should work well, no much resolutions anyway
-				// and will work better since this is human-tested!
-//				tv.setOffset( 0.00145f );	// 1920x1080
-				tv.setOffset( 0.002f );	// 1920x1080
-				tv.setDistortion( 0.2f );
-				tv.setTint( 0.95f, 0.8f, 1.0f );
-			}
+		if( crt != null ) {
+			startMs = TimeUtils.millis();
+			crt.setTime( 0 );
+
+			// FIXME should find out a way to compute this per-resolution! OR!
+			// Just try manually the game and write down the offset, this should work well, no much resolutions anyway
+			// and will work better since this is human-tested!
+			// tv.setOffset( 0.00145f ); // 1920x1080
+			crt.setOffset( 0.002f );	// 1920x1080
+			crt.setDistortion( 0.2f );
+			crt.setTint( 0.95f, 0.8f, 1.0f );
+		}
+
+		if( curvature != null ) {
+			curvature.setDistortion( 0.4f );
+			curvature.setZoom( 1f );
 		}
 	}
 
@@ -75,17 +84,6 @@ public final class AggressiveWarm implements Animator {
 
 	@Override
 	public void update( PlayerCar player, GhostCar ghost ) {
-		if( Config.PostProcessing.EnableCrtScreen ) {
-			// compute time (add noise)
-			float secs = (float)(TimeUtils.millis() - startMs) / 1000;
-			boolean randomNoiseInTime = false;
-			if( randomNoiseInTime ) {
-				tv.setTime( secs + MathUtils.random() / (MathUtils.random() * 64f + 0.001f) );
-			} else {
-				tv.setTime( secs );
-			}
-		}
-
 		if( player == null ) {
 			return;
 		}
@@ -100,23 +98,33 @@ public final class AggressiveWarm implements Animator {
 		float driftStrength = AMath.clamp( AMath.lerp( prevDriftStrength, player.driftState.driftStrength, 0.01f ), 0, 1 );
 		prevDriftStrength = driftStrength;
 
-
-		if( Config.PostProcessing.EnableZoomBlur && player != null ) {
-			zoom.setOrigin( playerScreenPos );
-			zoom.setStrength( -0.1f * player.carState.currSpeedFactor * timeFactor );
+		if( crt != null ) {
+			// compute time (add noise)
+			float secs = (float)(TimeUtils.millis() - startMs) / 1000;
+			boolean randomNoiseInTime = false;
+			if( randomNoiseInTime ) {
+				crt.setTime( secs + MathUtils.random() / (MathUtils.random() * 64f + 0.001f) );
+			} else {
+				crt.setTime( secs );
+			}
 		}
 
-		if( Config.PostProcessing.EnableBloom ) {
+		if( zoom != null && player != null ) {
+			float zoomfactor = timeFactor;// * player.carState.currSpeedFactor;
+			zoom.setOrigin( playerScreenPos );
+			zoom.setStrength( -0.1f * zoomfactor );
+			zoom.setZoom( 1.0f + 0.15f * zoomfactor );
+		}
+
+		if( bloom != null ) {
 			bloom.setBaseSaturation( AMath.lerp( 1, 0.15f, timeFactor ) );
 			// bloom.setBloomSaturation( 1.5f - factor * 0.85f ); // TODO when charged
 			// bloom.setBloomSaturation( 1.5f - factor * 1.5f ); // TODO when completely discharged
 			bloom.setBloomSaturation( 1f - timeFactor * 0.5f );
-
-			// testing camera motion
 			bloom.setThreshold( AMath.lerp( 0.4f, 0.45f, timeFactor ) );
 		}
 
-		if( Config.PostProcessing.EnableVignetting ) {
+		if( vignette != null ) {
 			// vignette.setY( (1 - factor) * 0.74f + factor * 0.4f );
 
 			if( vignette.controlSaturation ) {
@@ -125,14 +133,16 @@ public final class AggressiveWarm implements Animator {
 				vignette.setSaturationMul( 1f + timeFactor * 0.125f );
 			}
 
-//			 vignette.setCenter( playerScreenPos.x, playerScreenPos.y );
-//			 vignette.setCoords( 1.5f - driftStrength * 0.8f, 0.1f );
+			// vignette.setCenter( playerScreenPos.x, playerScreenPos.y );
+			// vignette.setCoords( 1.5f - driftStrength * 0.8f, 0.1f );
 
 			vignette.setLutIntensity( timeFactor * 1.25f );
-			vignette.setLutIndex( 7 );
-
 			vignette.setIntensity( timeFactor );
+		}
+
+		if( curvature != null ) {
+			curvature.setDistortion( 0.2f );
+			curvature.setZoom( 0.90f );
 		}
 	}
 }
-
