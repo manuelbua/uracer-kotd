926696bb24457ace14923767c2b19efaa36e2929
Manuel Bua <manuel.bua@gmail.com>
2012-06-20 18:24:44 +0200

    Remove unused code.

--- uracer-desktop/src/com/bitfire/uracer/game/rendering/GameRenderer.java
+++ uracer-desktop/src/com/bitfire/uracer/game/rendering/GameRenderer.java
@@ -23,8 +23,6 @@ public final class GameRenderer {
 	private final PostProcessor postProcessor;
 	private final GameWorldRenderer worldRenderer;
 
-	// private final FrameBuffer depthMap;
-
 	/** Manages to convert world positions expressed in meters or pixels to the corresponding position to screen pixels.
 	 * To use this class, the GameWorldRenderer MUST be already constructed and initialized. */
 	public static final class ScreenUtils {
@@ -72,17 +70,8 @@ public final class GameRenderer {
 		ScreenUtils.init( worldRenderer );
 		Gdx.app.log( "GameRenderer", "ScreenUtils " + (ScreenUtils.ready ? "initialized." : "NOT initialized!") );
 
-		// depth map support via auxiliary texture
-		// TODO make it optional
-		// if( true ) {
-		// depthMap = new FrameBuffer( Format.RGBA8888, width, height, true );
-		// }
-
 		// post-processing
-		boolean supports32Bpp = Config.isDesktop;
-		postProcessor = (createPostProcessor ? new PostProcessor( width, height, true /* depth */, false /* alpha */, supports32Bpp ) : null);
-		// postProcessor = (createPostProcessor ? new PostProcessor( Config.PostProcessing.PotRttFboWidth,
-		// Config.PostProcessing.PotRttFboWidth, true /* depth */, false /* alpha */, supports32Bpp ) : null);
+		postProcessor = (createPostProcessor ? new PostProcessor( width, height, true /* depth */, false /* alpha */, Config.isDesktop /* supports32Bpp */) : null);
 	}
 
 	public void dispose() {
@@ -108,42 +97,17 @@ public final class GameRenderer {
 		return worldRenderer;
 	}
 
-	// public Texture getDepthMap() {
-	// if( depthMap != null ) {
-	// return depthMap.getColorBufferTexture();
-	// }
-	//
-	// return null;
-	// }
-
-	// private void generateDepthMap() {
-	// // capture depth
-	// gl.glDepthMask( true );
-	// depthMap.begin();
-	// {
-	// float v = 0f;
-	// gl.glClearDepthf( v );
-	// // Gdx.gl.glClearColor( 1, 1, 1, 1 );
-	// Gdx.gl.glClearColor( v, v, v, v );
-	// Gdx.gl.glClear( GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT );
-	//
-	// // TODO implement box2d entities to render depth
-	// worldRenderer.renderAllMeshes( true );
-	// }
-	// depthMap.end();
-	// }
-
 	public void beforeRender( float timeAliasingFactor ) {
 		GameEvents.gameRenderer.timeAliasingFactor = timeAliasingFactor;
 		GameEvents.gameRenderer.trigger( this, GameRendererEvent.Type.OnSubframeInterpolate );
 	}
 
 	public void render() {
+		// postproc begins
 		boolean postProcessorReady = hasPostProcessor() && postProcessor.isEnabled();
 
 		if( postProcessorReady ) {
 
-			// generateDepthMap();
 			postProcessorReady = postProcessor.capture();
 			if( !postProcessorReady ) {
 				Gdx.app.error( "GameRenderer", "postprocessor::capture() error" );
@@ -154,6 +118,8 @@ public final class GameRenderer {
 			gl.glClearColor( 0, 0, 0, 0 );
 			gl.glClear( GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT );
 		}
+
+		// postproc ends
 
 		gl.glDepthMask( true );
 
@@ -181,29 +147,23 @@ public final class GameRenderer {
 		}
 		batchRenderer.end();
 
+		// postproc begins
 		gl.glDisable( GL20.GL_CULL_FACE );
 		if( world.isNightMode() ) {
-			if( Config.Graphics.NightAsOverlay ) {
-				if( postProcessorReady ) {
-					postProcessor.render();
-				}
-
-				worldRenderer.renderLigthMap( null );
+			// hook into the next PostProcessor source buffer (the last result)
+			// and blend the lightmap on it
+			if( postProcessorReady ) {
+				worldRenderer.renderLigthMap( postProcessor.captureEnd() );
+				postProcessor.render();
 			} else {
-				// hook into the next PostProcessor source buffer (the last result)
-				// and blend the lightmap on it
-				if( postProcessorReady ) {
-					worldRenderer.renderLigthMap( postProcessor.captureEnd() );
-					postProcessor.render();
-				} else {
-					worldRenderer.renderLigthMap( null );
-				}
+				worldRenderer.renderLigthMap( null );
 			}
 		} else {
 			if( postProcessorReady ) {
 				postProcessor.render();
 			}
 		}
+		// postproc ends
 	}
 
 	// manages and triggers debug event
