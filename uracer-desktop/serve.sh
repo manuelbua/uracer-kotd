f0ec11fc9ed92c20af35051c702e3771b420e811
Manuel Bua <manuel.bua@gmail.com>
2012-06-20 14:21:55 +0200

    Shows IP address when starting to serve.

--- uracer-desktop/serve.sh
+++ uracer-desktop/serve.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-echo "Serving on localhost:8000... (CTRL+C to stop)"
+IPADDR=`ip -4 -o addr|grep eth0|awk '{print $4}'|cut -d/ -f1`
+echo "Serving on ${IPADDR}:8000... (CTRL+C to stop)"
 webfsd -F -r ./build/
