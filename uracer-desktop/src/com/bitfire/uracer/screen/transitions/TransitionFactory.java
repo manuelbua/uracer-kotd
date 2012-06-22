56323581c89908c1138974169dddbcb343f745ef
Manuel Bua <manuel.bua@gmail.com>
2012-06-22 14:18:54 +0200

    [added] TransitionFactory now caches transitions for reuse.

--- uracer-desktop/src/com/bitfire/uracer/screen/transitions/TransitionFactory.java
+++ uracer-desktop/src/com/bitfire/uracer/screen/transitions/TransitionFactory.java
@@ -1,12 +1,36 @@
 package com.bitfire.uracer.screen.transitions;
 
+import com.badlogic.gdx.utils.LongMap;
+import com.bitfire.uracer.utils.Hash;
+
 public final class TransitionFactory {
 
+	private static LongMap<ScreenTransition> transitions = new LongMap<ScreenTransition>();
+
 	public enum TransitionType {
-		None, Fader
+		None, Fader;
+
+		public long hash;
+		private TransitionType() {
+			hash = Hash.APHash( this.name() );
+		}
 	}
 
-	public static ScreenTransition createTransition( TransitionType transitionType ) {
+	public static ScreenTransition getTransition( TransitionType transitionType ) {
+		ScreenTransition transition = null;
+
+		if( transitions.containsKey( transitionType.hash ) ) {
+			transition = transitions.get( transitionType.hash );
+			transition.reset();
+		} else {
+			transition = createTransition( transitionType );
+			transitions.put( transitionType.hash, transition );
+		}
+
+		return transition;
+	}
+
+	private static ScreenTransition createTransition( TransitionType transitionType ) {
 		ScreenTransition transition = null;
 
 		switch( transitionType ) {
@@ -20,5 +44,11 @@ public final class TransitionFactory {
 		}
 
 		return transition;
+	}
+
+	public static void dispose() {
+		for( ScreenTransition t : transitions.values() ) {
+			t.dispose();
+		}
 	}
 }
