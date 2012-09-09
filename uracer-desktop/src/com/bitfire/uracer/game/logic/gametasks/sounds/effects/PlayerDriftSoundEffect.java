796feec7ba12703af911b57829efdd332480dbc5
Manuel Bua <manuel.bua@gmail.com>
2012-09-09 21:55:31 +0200

    Minor sound modifications

--- uracer-desktop/src/com/bitfire/uracer/game/logic/gametasks/sounds/effects/PlayerDriftSoundEffect.java
+++ uracer-desktop/src/com/bitfire/uracer/game/logic/gametasks/sounds/effects/PlayerDriftSoundEffect.java
@@ -21,7 +21,7 @@ public final class PlayerDriftSoundEffect extends SoundEffect {
 	private long driftId = -1, lastDriftId = -1;
 	private float driftLastPitch = 0;
 	private static final float pitchFactor = 1f;
-	private static final float pitchMin = 0.75f;
+	private static final float pitchMin = 0.7f;
 	private static final float pitchMax = 1f;
 
 	private boolean doFadeIn = false;
