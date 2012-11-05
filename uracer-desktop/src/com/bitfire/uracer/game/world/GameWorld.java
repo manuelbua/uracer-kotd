7ac1f9f5e6abf2df7a3a8df2b8dd087a6e442372
Manuel Bua <manuel.bua@gmail.com>
2012-11-05 23:11:19 +0100

    [update] track, refactor central trees [update] player headlights should pass through holes in walls

--- uracer-desktop/src/com/bitfire/uracer/game/world/GameWorld.java
+++ uracer-desktop/src/com/bitfire/uracer/game/world/GameWorld.java
@@ -221,12 +221,13 @@ public final class GameWorld {
 		playerHeadlightsA = new ConeLight(rayHandler, maxRays, c, 25, 0, 0, 0, 9);
 		playerHeadlightsA.setSoft(true);
 // playerHeadlightsA.setMaskBits(CollisionFilters.CategoryTrackWalls | CollisionFilters.CategoryReplay);
-		playerHeadlightsA.setMaskBits(CollisionFilters.CategoryTrackWalls);
+// playerHeadlightsA.setMaskBits(CollisionFilters.CategoryTrackWalls);
+		playerHeadlightsA.setMaskBits(0);
 
 		playerHeadlightsB = new ConeLight(rayHandler, maxRays, c, 25, 0, 0, 0, 9);
 		playerHeadlightsB.setSoft(true);
 // playerHeadlightsB.setMaskBits(CollisionFilters.CategoryTrackWalls | CollisionFilters.CategoryReplay);
-		playerHeadlightsB.setMaskBits(CollisionFilters.CategoryTrackWalls);
+		playerHeadlightsB.setMaskBits(0);
 
 		// setup level lights data, if any
 		Vector2 pos = new Vector2();
