91d6b6ec91c041904074b1eb4277895ab023f255
Manuel Bua <manuel.bua@gmail.com>
2013-05-21 20:10:30 +0200

    More zoom-in, minor refactoring, update tracks

--- uracer-desktop/src/com/bitfire/uracer/game/rendering/GameWorldRenderer.java
+++ uracer-desktop/src/com/bitfire/uracer/game/rendering/GameWorldRenderer.java
@@ -106,7 +106,7 @@ public final class GameWorldRenderer {
 	private CameraController camController;
 	private static final float CamPerspPlaneNear = 0.001f;
 	public static final float CamPerspPlaneFar = 240f;
-	public static final float MaxCameraZoom = 1.4f;
+	public static final float MaxCameraZoom = 1.5f;
 	public static final float CamPerspElevation = 100f;
 	private final float DefaultSsaoScale = 1f / 48f;
 
