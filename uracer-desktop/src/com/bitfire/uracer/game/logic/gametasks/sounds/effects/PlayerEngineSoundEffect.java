dff960a4856b518b1a2d54a05e5febee24b4a2bc
Manuel Bua <manuel.bua@gmail.com>
2013-07-24 23:15:02 +0200

    Start car engine implementation, add preliminary engine samples, classes

--- uracer-desktop/src/com/bitfire/uracer/game/logic/gametasks/sounds/effects/PlayerEngineSoundEffect.java
+++ uracer-desktop/src/com/bitfire/uracer/game/logic/gametasks/sounds/effects/PlayerEngineSoundEffect.java
@@ -2,22 +2,18 @@
 package com.bitfire.uracer.game.logic.gametasks.sounds.effects;
 
 import com.badlogic.gdx.audio.Sound;
-import com.bitfire.uracer.game.logic.gametasks.SoundManager;
 import com.bitfire.uracer.game.logic.gametasks.sounds.SoundEffect;
 import com.bitfire.uracer.game.player.PlayerCar;
 import com.bitfire.uracer.resources.Sounds;
-import com.bitfire.uracer.utils.AMath;
 
 public final class PlayerEngineSoundEffect extends SoundEffect {
-	private Sound carEngine = null;
-	private long carEngineId = -1;
-	private float carEnginePitchStart = 0;
-	private float carEnginePitchLast = 0;
-	private static final float CarEnginePitchMin = 0.8f;
-	private boolean started;
+	private int NumTracks = 7;
+	private Sound[] engine = new Sound[NumTracks];
+	private long[] mid = new long[NumTracks];
+	private boolean[] started = new boolean[NumTracks];
 
 	public PlayerEngineSoundEffect () {
-		carEngine = Sounds.carEngine;
+		engine = Sounds.carEngine;
 	}
 
 	@Override
@@ -27,44 +23,50 @@ public final class PlayerEngineSoundEffect extends SoundEffect {
 
 	@Override
 	public void tick () {
-		if (!isPaused && hasPlayer && carEngineId > -1) {
-			float speedFactor = player.carState.currSpeedFactor;
+		float speedFactor = player.carState.currSpeedFactor;
 
-			float pitch = CarEnginePitchMin + speedFactor * 1.25f;
+		// idle
+		setVolume(0, 0.3f);
+		setPitch(0, 1f); // 1 -> 1.85f (switch to #1)
 
-			// float pitch = carEnginePitchMin + 0.8f;
-			if (!AMath.equals(pitch, carEnginePitchLast)) {
-				carEngine.setPitch(carEngineId, pitch);
-				carEnginePitchLast = pitch;
-			}
-		}
-	}
+		// on, low
+		setVolume(1, 0.3f);
+		setPitch(1, 1f); // 1 -> 1.5f (switch to #2)
 
-	private void start () {
-		if (started || !hasPlayer) {
-			return;
-		}
-
-		started = true;
-		carEngineId = loop(carEngine, SoundManager.SfxVolumeMul);
-		carEngine.setPitch(carEngineId, carEnginePitchStart);
+		// // idle
+		// setVolume(0, 0.3f);
+		// setPitch(0, 1f); // 1 -> 1.85f (switch to #1)
+		//
+		// // on, low
+		// setVolume(1, 0.3f);
+		// setPitch(1, 1f); // 1 -> 1.5f (switch to #2)
+		//
+		// setVolume(4, 0f);
+		// setPitch(4, 1.5f);
+		//
+		// // on, mid
+		// setVolume(2, 0f);
+		// setPitch(2, 1f); // 1 -> 1.33f (switch to #3)
+		// setVolume(5, 0f);
+		// setPitch(5, 1f);
+		//
+		// // on, high
+		// setVolume(3, 0f);
+		// setPitch(3, 1f);
+		// setVolume(6, 0f);
+		// setPitch(6, 1f);
 	}
 
 	@Override
 	public void stop () {
-		if (!started) {
-			return;
+		for (int i = 0; i < NumTracks; i++) {
+			stop(i);
 		}
-
-		carEngine.stop();
-		started = false;
 	}
 
 	@Override
 	public void gameReset () {
 		stop();
-		carEnginePitchStart = CarEnginePitchMin;
-		carEnginePitchLast = CarEnginePitchMin;
 	}
 
 	@Override
@@ -76,5 +78,42 @@ public final class PlayerEngineSoundEffect extends SoundEffect {
 		} else {
 			stop();
 		}
+	}
+
+	private void start (int track) {
+		if (started[track]) {
+			return;
+		}
+
+		started[track] = true;
+		mid[track] = loop(engine[track], 0);
+		setVolume(track, 0);
+	}
+
+	private void start () {
+		for (int i = 0; i < NumTracks; i++) {
+			start(i);
+		}
+	}
+
+	public void stop (int track) {
+		if (!started[track]) {
+			return;
+		}
+
+		started[track] = false;
+		if (mid[track] > -1) {
+			engine[track].stop(mid[track]);
+		}
+	}
+
+	//
+
+	private void setVolume (int track, float vol) {
+		engine[track].setVolume(mid[track], vol);
+	}
+
+	private void setPitch (int track, float pitch) {
+		engine[track].setPitch(mid[track], pitch);
 	}
 }
