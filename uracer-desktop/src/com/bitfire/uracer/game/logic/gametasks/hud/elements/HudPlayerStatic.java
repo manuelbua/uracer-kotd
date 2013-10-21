277caa3454a67036e4d499c3abcbbcb3a76358d8
Manuel Bua <manuel.bua@gmail.com>
2013-10-21 21:51:05 +0200

    Do not show speed/meters anymore

--- uracer-desktop/src/com/bitfire/uracer/game/logic/gametasks/hud/elements/HudPlayerStatic.java
+++ uracer-desktop/src/com/bitfire/uracer/game/logic/gametasks/hud/elements/HudPlayerStatic.java
@@ -2,26 +2,22 @@
 package com.bitfire.uracer.game.logic.gametasks.hud.elements;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.MathUtils;
-import com.bitfire.uracer.configuration.Config;
 import com.bitfire.uracer.configuration.UserProfile;
 import com.bitfire.uracer.game.logic.gametasks.hud.HudElement;
-import com.bitfire.uracer.game.logic.gametasks.hud.HudLabel;
 import com.bitfire.uracer.game.logic.gametasks.hud.elements.player.BasicInfo;
-import com.bitfire.uracer.resources.BitmapFontFactory.FontFace;
-import com.bitfire.uracer.utils.CarUtils;
 
 public class HudPlayerStatic extends HudElement {
 	private BasicInfo basicInfo;
-	private HudLabel labelSpeed, labelDistance;
+
+	// private HudLabel labelSpeed, labelDistance;
 
 	public HudPlayerStatic (UserProfile userProfile) {
 		basicInfo = new BasicInfo(userProfile);
-		labelSpeed = new HudLabel(FontFace.Roboto, "", true);
-		labelSpeed.setPosition(Config.Graphics.ReferenceScreenWidth - 190, Config.Graphics.ReferenceScreenHeight - 110);
-		labelDistance = new HudLabel(FontFace.Roboto, "", true);
-		labelDistance.setScale(0.85f);
-		labelDistance.setPosition(Config.Graphics.ReferenceScreenWidth - 190, Config.Graphics.ReferenceScreenHeight - 50);
+		// labelSpeed = new HudLabel(FontFace.Roboto, "", true);
+		// labelSpeed.setPosition(Config.Graphics.ReferenceScreenWidth - 190, Config.Graphics.ReferenceScreenHeight - 110);
+		// labelDistance = new HudLabel(FontFace.Roboto, "", true);
+		// labelDistance.setScale(0.85f);
+		// labelDistance.setPosition(Config.Graphics.ReferenceScreenWidth - 190, Config.Graphics.ReferenceScreenHeight - 50);
 	}
 
 	@Override
@@ -34,11 +30,11 @@ public class HudPlayerStatic extends HudElement {
 		if (hasPlayer) {
 			basicInfo.render(batch);
 
-			labelSpeed.setString(MathUtils.round(CarUtils.mtSecToKmHour(player.getInstantSpeed())) + " kmh");
-			labelSpeed.render(batch);
-
-			labelDistance.setString(MathUtils.round(player.getTraveledDistance()) + " mt\n");
-			labelDistance.render(batch);
+			// labelSpeed.setString(MathUtils.round(CarUtils.mtSecToKmHour(player.getInstantSpeed())) + " kmh");
+			// labelSpeed.render(batch);
+			//
+			// labelDistance.setString(MathUtils.round(player.getTraveledDistance()) + " mt\n");
+			// labelDistance.render(batch);
 		}
 	}
 }
