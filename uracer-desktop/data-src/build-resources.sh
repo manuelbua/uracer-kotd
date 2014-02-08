22dfd1251d0302728eb4fb02c96ae05410fc7621
Manuel Bua <manuel.bua@gmail.com>
2014-02-08 17:30:00 +0100

    Update to latest libgdx

--- uracer-desktop/data-src/build-resources.sh
+++ uracer-desktop/data-src/build-resources.sh
@@ -4,7 +4,7 @@ CLASS_PATH="/home/manuel/dev/libgdx/dist"
 JARS="${CLASS_PATH}/gdx.jar:${CLASS_PATH}/gdx-natives.jar:${CLASS_PATH}/gdx-backend-lwjgl.jar:${CLASS_PATH}/gdx-backend-lwjgl-natives.jar"
 
 GDX_TOOLS_PATH="/home/manuel/dev/libgdx/dist/extensions/gdx-tools/"
-TEX_PACKER="java -classpath ${JARS}:${GDX_TOOLS_PATH}/gdx-tools.jar com.badlogic.gdx.tools.imagepacker.TexturePacker2"
+TEX_PACKER="java -classpath ${JARS}:${GDX_TOOLS_PATH}/gdx-tools.jar com.badlogic.gdx.tools.texturepacker.TexturePacker"
 
 # in uracer/libs
 #GDX_TILED_PREP_PATH="/home/manuel/dev/uracer-libgdx/libs/gdx-tiled-preprocessor/bin"
