eb26ab98a6949975e3a5ceedd29c3be30d519b63
Manuel Bua <manuel.bua@gmail.com>
2014-03-18 23:47:10 +0100

    Minor changes to effect

--- uracer-desktop/src/com/bitfire/uracer/game/logic/post/animators/DefaultAnimator.java
+++ uracer-desktop/src/com/bitfire/uracer/game/logic/post/animators/DefaultAnimator.java
@@ -368,14 +368,14 @@ public final class DefaultAnimator implements PostProcessingAnimator {
 
 			// Gdx.app.log("", "pdist=" + pdist);
 
-			shafts.setThreshold(0.8f);
+			shafts.setThreshold(0.9f);
 			shafts.setDensity(0.84f);
 			shafts.setExposure(0.08f);
 			shafts.setWeight(1);
-			shafts.setDecay(0.92f);
+			shafts.setDecay(0.96f);
 			Combine combine = shafts.getCombinePass();
 			combine.setSource2Intensity(1);
-			combine.setSource2Saturation(0.6f);
+			combine.setSource2Saturation(0.75f);
 
 			// Gdx.app.log("", "speed=" + sfactor);
 			// shafts.setLightScreenPositionN(0.5f, 0.3f);
