153d31c8b7a876bbf59ae31b51346c271f891983
Manuel Bua <manuel.bua@gmail.com>
2018-04-22 20:21:59 +0200

    Properly mark class as deprecated

--- uracer-desktop/src/com/bitfire/postprocessing/effects/LensFlare.java
+++ uracer-desktop/src/com/bitfire/postprocessing/effects/LensFlare.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2012 tsagrista
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,6 +23,7 @@ import com.bitfire.postprocessing.filters.Lens;
 /** Lens flare effect.
  * @deprecated Please use the better {@link com.bitfire.postprocessing.effects.LensFlare2}.
  * @author Toni Sagrista */
+@Deprecated
 public final class LensFlare extends PostProcessorEffect {
 	private Lens lens = null;
 
